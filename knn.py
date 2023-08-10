ea41037e126e3865287336eb1ea18b21a9c82cc2
Carlos <carlosmgov7k@gmail.com>
2023-08-10 13:22:49 +0200

    read train data file just right

--- knn.py
+++ knn.py
@@ -17,15 +17,20 @@ TEST_LABELS_FILE = DATA_DIR + 't10k-labels.idx1-ubyte'
 TRAIN_DATA_FILE = DATA_DIR + 'train-images.idx3-ubyte'
 TRAIN_LABELS_FILE = DATA_DIR + 'train-labels.idx1-ubyte'
 
+# To interpret bytes as an integer, since python doesnt support it
+def byte_to_int (b):
+    return int.from_bytes(b, 'big') # Big/Little endian stands for bit readwise
+
+
 # Read the images in file specified
 def read_images(file):
     images = []
     # rb for read binary
     with open(file, 'rb') as f:
         _ = f.read(4) # Reading 4 bytes that stands for Magic number. Not interested in using it.
-        num_images = f.read(4)
-        num_rows = f.read(4)
-        num_columns = f.read(4)
+        num_images = byte_to_int(f.read(4))
+        num_rows = byte_to_int(f.read(4))
+        num_columns = byte_to_int(f.read(4))
         for image_idx in range(num_images):
             image = []
             for row_idx in range(num_rows):
@@ -35,15 +40,15 @@ def read_images(file):
                     row.append(pixel)
                 image.append(row)
             images.append(image)
-
+        return images
 
 
 
 def main():
     img_train = read_images(TRAIN_DATA_FILE)
-    lab_train = read_images(TRAIN_LABELS_FILE)
-    img_test = read_images(TEST_DATA_FILE)
-    lab_test = read_images(TEST_LABELS_FILE)
+    # lab_train = read_images(TRAIN_LABELS_FILE)
+    # img_test = read_images(TEST_DATA_FILE)
+    # lab_test = read_images(TEST_LABELS_FILE)
     print(len(img_train))
 
 # Allows the application to run as a script but not as a module. Python convention.
